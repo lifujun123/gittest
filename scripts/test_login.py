ca0a027ce94ae5076c43e3d8279f3eccc9820e3a
lifujun123 <1024491015@qq.com>
2021-11-28 14:54:11 +0800

    first commit

---
+++ scripts/test_login.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+class TestLogin():
+    def test_1(self):
+        print("test_1")
+        assert 1
+
+    def test_2(self):
+        print("test_2")
+        assert 1
+
+    def test_3(self):
+        print("test_3")
+        assert 0
+
+    def test_4(self):
+        print("test_4")
+        assert 1
